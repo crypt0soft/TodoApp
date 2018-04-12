13d1ccd6362581b7efc72b1b2ee3e329e18e250c
Lopez deleon <ruben.lopez.deleon@accenture.com>
2018-04-12 13:38:04 -0300

    initial cordova app

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,29 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-appcenter-analytics.Analytics",
+    "file": "plugins/cordova-plugin-appcenter-analytics/www/Analytics.js",
+    "pluginId": "cordova-plugin-appcenter-analytics",
+    "clobbers": [
+      "AppCenter.Analytics"
+    ]
+  },
+  {
+    "id": "cordova-plugin-appcenter-crashes.Crashes",
+    "file": "plugins/cordova-plugin-appcenter-crashes/www/Crashes.js",
+    "pluginId": "cordova-plugin-appcenter-crashes",
+    "clobbers": [
+      "AppCenter.Crashes"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-appcenter-shared": "0.1.3",
+  "cordova-plugin-appcenter-analytics": "0.1.3",
+  "cordova-plugin-appcenter-crashes": "0.1.3"
+};
+// BOTTOM OF METADATA
+});
